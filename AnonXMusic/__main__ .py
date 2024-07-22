0571b14281c10cd8f28a045567b9d8b8ac83c897
𝗩𝗔𝗠𝗦𝗜 𝗥𝗘𝗗𝗗𝗬 <157066626+Noobcoder-xD@users.noreply.github.com>
2024-07-22 13:27:01 +0530

    Update __main__ .py

--- AnonXMusic/__main__ .py
+++ AnonXMusic/__main__ .py
@@ -12,8 +12,8 @@ from AnonXMusic.plugins import ALL_MODULES
 from AnonXMusic.utils.database import get_banned_users, get_gbanned
 from config import BANNED_USERS
 
-
 async def init():
+    # Check if assistant client variables are defined
     if (
         not config.STRING1
         and not config.STRING2
@@ -23,39 +23,57 @@ async def init():
     ):
         LOGGER(__name__).error("Assistant client variables not defined, exiting...")
         exit()
-    await sudo()
+
+    await sudo()  # Perform sudo initialization
+
     try:
+        # Fetch global banned users and add to BANNED_USERS set
         users = await get_gbanned()
         for user_id in users:
             BANNED_USERS.add(user_id)
+
+        # Fetch banned users and add to BANNED_USERS set
         users = await get_banned_users()
         for user_id in users:
             BANNED_USERS.add(user_id)
     except:
         pass
-    await app.start()
+
+    await app.start()  # Start the main bot client
+
+    # Import all modules
     for all_module in ALL_MODULES:
         importlib.import_module("AnonXMusic.plugins" + all_module)
     LOGGER("AnonXMusic.plugins").info("Successfully Imported Modules...")
-    await userbot.start()
-    await Anony.start()
+
+    await userbot.start()  # Start the userbot client
+    await Anony.start()  # Start the Anony client
+
     try:
+        # Attempt to stream a video call
         await Anony.stream_call("https://telegra.ph/file/cba632240b79207bf8a9c.mp4")
     except NoActiveGroupCall:
+        # Log error if no active group call and exit
         LOGGER("AnonXMusic").error(
             "Please turn on the videochat of your log group\channel.\n\nStopping Bot..."
         )
         exit()
     except:
         pass
-    await Anony.decorators()
+
+    await Anony.decorators()  # Setup decorators
+
+    # Log a message indicating bot is running
     LOGGER("AnonXMusic").info("╔═════ஜ۩۞۩ஜ════╗\n  ♨️𝗠𝗔𝗗𝗘 𝗕𝗬 𝐂𝐡𝐢𝐧𝐧𝐚 ♨️\n╚═════ஜ۩۞۩ஜ════╝")
         
-    await idle()
+    await idle()  # Keep the bot running
+
+    # Stop the bot and userbot gracefully
     await app.stop()
     await userbot.stop()
-    LOGGER("AnonXMusic").info("╔═════ஜ۩۞۩ஜ════╗\n  ♨️𝗠𝗔𝗗𝗘 𝗕𝗬 𝐂𝐡𝐢𝐧𝐧𝐚 ♨️\n╚═════ஜ۩۞۩ஜ════╝")
 
+    # Log a closing message
+    LOGGER("AnonXMusic").info("╔═════ஜ۩۞۩ஜ════╗\n  ♨️𝗠𝗔𝗗𝗘 𝗕𝗬 𝐂𝐡𝐢𝐧𝐧𝐚 ♨️\n╚═════ஜ۩۞۩ஜ════╝")
 
 if __name__ == "__main__":
     asyncio.get_event_loop().run_until_complete(init())
